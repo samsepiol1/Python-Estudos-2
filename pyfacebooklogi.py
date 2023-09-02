862407f057810ecb42f0bf111b91c01c9bd36bbb
Lucas Matheus <49918079+samsepiol1@users.noreply.github.com>
2023-09-02 18:03:53 -0300

    Update pyfacebooklogi.py

--- pyfacebooklogi.py
+++ pyfacebooklogi.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
-browser=webdriver.Chrome('C:/Users/Deus/Downloads/chromedriver.exe')
+browser=webdriver.Chrome('')
 browser.get('http://www.facebook.com')
 Searchbar1=browser.find_element_by_id('email')
-Searchbar1.send_keys('steve;jkl5@gmail.com')
+Searchbar1.send_keys('')
 Searchbar2=browser.find_element_by_id('pass')
-Searchbar2.send_keys('imac88246948')
+Searchbar2.send_keys('')
 Searchbar3=browser.find_element_by_id('u_0_0')
 from selenium.webdriver.common.keys import Keys
 Searchbar3.send_keys(Keys.ENTER)

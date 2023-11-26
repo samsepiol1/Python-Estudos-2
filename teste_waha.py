420d77d1b895b673b56a4b050ca6a309cd4cf092
Lucas Matheus <49918079+samsepiol1@users.noreply.github.com>
2023-11-26 16:36:40 -0300

    Update teste_waha.py

--- teste_waha.py
+++ teste_waha.py
@@ -1,13 +1,13 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:/Users/Deus.DEUS.002/Documents/site_final/img/chromedriver.exe')
-driver.get('https://waha.teleperformance.com.br/vpn/index.html')
+driver = webdriver.Chrome('')
+driver.get('')
 Searchbar1 = driver.find_element_by_id('Enter user name')
 time.sleep(0.5)
-Searchbar1.send_keys('silva.16955')
+Searchbar1.send_keys('')
 Searchbar2 = driver.find_element_by_id('passwd')
-Searchbar2.send_keys('mudar@123')
+Searchbar2.send_keys('')
 Searchbar3 = driver.find_element_by_id('Log_On')
 Searchbar3.click()
 
